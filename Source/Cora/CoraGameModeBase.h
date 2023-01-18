8baa17019d96e532bff197b8977d10885bcfb18c
Gwenaelle <75257467+gwendvr@users.noreply.github.com>
2023-01-18 11:17:20 +0100

    add project

---
+++ Source/Cora/CoraGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CORA_API ACoraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
